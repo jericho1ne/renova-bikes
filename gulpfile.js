9d093d4f6d368ef28c97799bc058cf8f303c104e
Mihai Peteu <jericho1ne@yahoo.com>
2016-12-14 23:42:26 -0800

    gulpfile modified to use src and dest directories

--- gulpfile.js
+++ gulpfile.js
@@ -8,32 +8,38 @@ var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
 
-// Set the banner content
+var path = {
+    src: 'src/',
+    dest: 'app/',
+    jquery: 'node_modules/jquery',
+    bootstrap: 'node_modules/bootstrap',
+};
+
+// Banner
 var banner = ['/*!\n',
     ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
     ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
     ' */\n',
     ''
 ].join('');
 
-// Compile LESS files from /less into /css
+// Compile LESS files from /less into /css in the source dir
 gulp.task('less', function() {
     return gulp.src('src/less/agency.less')
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('src/css'))
+        .pipe(gulp.dest(path.src + '/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
 });
 
-// Minify compiled CSS
+// Minify compiled CSS - from source to dest
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('src/css/agency.css')
+    return gulp.src(path.src + '/css/agency.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('src/css'))
+        .pipe(gulp.dest(path.dest + '/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -45,7 +51,7 @@ gulp.task('minify-js', function() {
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('src/js'))
+        .pipe(gulp.dest(path.dest + '/js'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -53,11 +59,11 @@ gulp.task('minify-js', function() {
 
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('src/vendor/bootstrap'))
+    gulp.src([path.bootstrap + '/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest(path.dest + '/vendor/bootstrap'))
 
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('src/vendor/jquery'))
+    gulp.src([path.jquery + '/dist/jquery.js', path.jquery + '/dist/jquery.min.js'])
+        .pipe(gulp.dest(path.dest + '/vendor/jquery'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -67,7 +73,17 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('src/vendor/font-awesome'))
+        .pipe(gulp.dest(path.dest + '/vendor/font-awesome'))
+
+    gulp.src([
+    		path.src + '/**/*.html', 
+    		path.src + '/**/*.php',
+    		path.src + '/**/*.js',
+    	])
+        .pipe(gulp.dest(path.dest))
+
+    gulp.src([path.src + '/css/*.css']).pipe(gulp.dest(path.dest + '/css'))
+    gulp.src([path.src + '/img/*']).pipe(gulp.dest(path.dest + '/img'))
 })
 
 // Run everything
